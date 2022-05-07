e971ceb68aad1cab26af3494268fa3b6d5ff1cfe
Martin Vasconcelo <martin.vasconcelo@gmail.com>
2022-05-06 23:27:00 -0300

    Pruebas simples 1.0

---
+++ cypress/integration/2-advanced-examples/PrimerasPruebas.js
@@ -0,0 +1,24 @@
+
+
+//Suite de casos de pruebas
+describe('Primer conjunto de casos de pruebas', function() {
+
+    //Caso de prueba 1
+    it('Ingresar a la pagina principal de DemoBlaze Electro', function(){
+        //Paso 1
+        //Paso 2
+        //Paso ...
+        cy.visit("https://www.demoblaze.com/prod.html?idp_=8")
+
+    })
+
+    //Caso de prueba 2
+
+
+
+
+    //Caso de prueba 3
+
+
+
+})
