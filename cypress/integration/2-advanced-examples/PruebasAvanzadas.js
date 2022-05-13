3d064bd2f104cd5c71cb0cebef03675d0a856593
Martin Vasconcelo <martin.vasconcelo@gmail.com>
2022-05-13 00:09:07 -0300

    Comenzando nueva pagina de pruebas

---
+++ cypress/integration/2-advanced-examples/PruebasAvanzadas.js
@@ -0,0 +1,17 @@
+/// <reference types="Cypress"/>
+
+// Suite de cassos de prueba 2.0
+describe("Tercer conjunto de casos de prueba", function () {
+
+    beforeEach( function () {
+        //Ingresamos a la pagina 
+        cy.visit("https://demo.opencart.com/index.php");
+      });
+
+
+      //Caso de prueba
+      it('Realizar una compra de celulares basadas en su titulo', function(){
+            
+      })
+
+})
